4622faaea210ebeca59673a29cfbdcc5ced39f16
Farhan Malhi <farhan.malhi@gmail.com>
2025-05-07 00:33:09 +0200

    fix: security commented

--- config/server.ts
+++ config/server.ts
@@ -4,32 +4,32 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  security: {
-    csrf: {
-      enabled: true,
-      key: '_csrf',
-      secret: env('CSRF_SECRET', 'your-csrf-secret'),
-    },
-    xframe: {
-      enabled: true,
-      value: 'SAMEORIGIN',
-    },
-    hsts: {
-      enabled: true,
-      maxAge: 31536000,
-      includeSubDomains: true,
-    },
-    xss: {
-      enabled: true,
-      mode: 'block',
-    },
-    cors: {
-      enabled: true,
-      origin: '*',
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      credentials: true,
-      maxAge: 86400,
-    },
-  },
+  // security: {
+  //   csrf: {
+  //     enabled: true,
+  //     key: '_csrf',
+  //     secret: env('CSRF_SECRET', 'your-csrf-secret'),
+  //   },
+  //   xframe: {
+  //     enabled: true,
+  //     value: 'SAMEORIGIN',
+  //   },
+  //   hsts: {
+  //     enabled: true,
+  //     maxAge: 31536000,
+  //     includeSubDomains: true,
+  //   },
+  //   xss: {
+  //     enabled: true,
+  //     mode: 'block',
+  //   },
+  //   cors: {
+  //     enabled: true,
+  //     origin: '*',
+  //     headers: '*',
+  //     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  //     credentials: false,
+  //     maxAge: 86400,
+  //   },
+  // },
 });
