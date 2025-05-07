3bfa187307af4798ead13c2d9d372baa9ea9826a
Farhan Malhi <farhan.malhi@gmail.com>
2025-05-07 10:14:49 +0200

    fix: logging disabled.

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  logger: {
-    level: 'debug',
-    requests: true,
+    keys: env.array("APP_KEYS"),
   },
 });
